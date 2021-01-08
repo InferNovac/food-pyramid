202c8e6c686159cdd183601b9134a0ad0baca5c8
InferNovac <45517444+InferNovac@users.noreply.github.com>
2021-01-08 00:24:14 +0000

    added appropiate buttons

--- src/javascript/components/FoodPyramid.js
+++ src/javascript/components/FoodPyramid.js
@@ -27,15 +27,46 @@ const calculateSections = (blocks) => {
     return foodBlocks;
 };
 
+const increaseListener = (intakeId) => {
+    const intake = document.getElementById(intakeId);
+    const value = parseInt(intake.innerText);
+    intake.innerText = value + 1;
+};
+
+const decreaseListener = (intakeId) => {
+    const intake = document.getElementById(intakeId);
+    const value = intake.innerText;
+    if (value > 0) {
+        intake.innerText = value - 1;
+    }
+};
+
+const intakeUI = (id) => {
+    const intakeId = `intake${id}`;
+    const increase = document.createElement("button");
+    const intake = document.createElement("div");
+    const decrease = document.createElement("button");
+    intake.setAttribute("id", intakeId);
+    intake.classList.add("space-between");
+    increase.textContent = "+";
+    intake.textContent = "0";
+    decrease.textContent = "-";
+    increase.addEventListener("click", () => increaseListener(intakeId));
+    decrease.addEventListener("click", () => decreaseListener(intakeId));
+    return [increase, intake, decrease];
+};
+
 export const constructPyramid = (blocks) => {
     const foodBlocks = calculateSections(blocks);
-    return foodBlocks
-        .map(
-            (block) =>
-                `
-            <div class="pyramid-part" title="${block.title}" style="${block.style}">
-            </div>
-        `
-        )
-        .join("");
+    return foodBlocks.map((block, index) => {
+        const foodBlock = document.createElement("div");
+        foodBlock.title = block.title;
+        foodBlock.style = block.style;
+        foodBlock.classList.add("pyramid-part");
+        const UI = intakeUI(index);
+        UI.map((element) => {
+            foodBlock.appendChild(element);
+        });
+        return foodBlock;
+    });
 };
--- src/javascript/main.js
+++ src/javascript/main.js
@@ -4,10 +4,13 @@ import { constructPyramid } from "./components/FoodPyramid";
 const container = document.getElementById("pyramid");
 
 const blocks = [
-    { value: 1, title: "Grains", color: "#e74c3c" },
-    { value: 3, title: "Fruit & Vegetables", color: "#f39c12" },
+    { value: 3, title: "Grains", color: "#e74c3c" },
+    { value: 4, title: "Fruit & Vegetables", color: "#f39c12" },
     { value: 5, title: "Diary & Meat", color: "#f1c40f" },
     { value: 11, title: "Grass, Oil & Sweets", color: "#3498db" },
 ];
 
-container.innerHTML = constructPyramid([...blocks]);
+const pyramid = constructPyramid([...blocks]);
+pyramid.map((foodBlock) => {
+    container.appendChild(foodBlock);
+});
